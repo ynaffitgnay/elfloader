6e0443a40c1dd43e3587f39787afa81341b4bc7f
Tiffany Yang <tiffanyyang@utexas.edu>
2019-04-03 23:24:17 -0600

    Use apager to figure out layout of program

--- code/pagers/apager.c
+++ code/pagers/apager.c
@@ -1,16 +1,59 @@
 #include <inttypes.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "loader_utils.h"
+#include "loader_elf.h"
 
 int main( int argc, char** argv, char** envp )
 {
+  Elf64_auxv_t *auxv;
   printf( "argv: %#" PRIx64
           "\t contents: %s\n", (uint64_t)argv, *argv );
   ++argv;
   printf( "++argv: %#" PRIx64
           "\t contents: %s\n", (uint64_t)argv, *argv );
 
-          
+  char** orig_envp = envp;
+  int num_env_vars = 0;
+  do {
+    printf( "env var: %s\n", *envp );
+    num_env_vars++;
+  } while (*envp++ != NULL);
+
+  num_env_vars -= 1;  // account for the extra null
+
+  char* final_string_addr = envp[num_env_vars - 1];
+  unsigned long chars_in_final_string = strlen(final_string_addr);
+  printf("strlen of last value @ envp: %lu\n", chars_in_final_string );
+  printf("address of final_string_addr + strlen = %lx\n",
+         (unsigned long)final_string_addr + chars_in_final_string );
+
+  printf( "total number of environment variables: %d\n",
+          num_env_vars );
+  
+  char* ptr = (char*) envp;
+  printf("sp: %#" PRIx64
+         " end o auxv: %#" PRIx64
+         " \n", (uint64_t)ptr, (uint64_t)(ptr + 19 * sizeof( Elf64_auxv_t )));
+
+  ptr += 19 * sizeof( Elf64_auxv_t );
+  printf("length of random bytes + cap string: %lu\n", strlen(ptr));
+
+  /* see which entries we have */
+
+  for (auxv = (Elf64_auxv_t*)envp; auxv->a_type != AT_NULL; auxv++) {
+    printf("aux type: %" PRIu64
+           "\n", auxv->a_type );
+  }
+
+  char buffer[17];
+  char* sp = (char*)(++auxv);  // end of auxiliary vector (after null)
+  strncpy(buffer, sp, 16);
+  buffer[16] = '\0';  
+  printf("sp: %#" PRIx64 " random bytes:  %s \n", (uint64_t)sp, buffer);
+  sp += 16;
+  printf("sp: %#" PRIx64 " capability string? %s \n", (uint64_t)sp, sp);
+  //printf("capability string now? %s \n", *(char*)(auxv++));
   return 0;
 }
