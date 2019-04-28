bba4e798ba945cdcb20f271ab9cfbf7737654832
Tiffany Yang <tiffanyyang@utexas.edu>
2019-04-28 17:32:07 -0500

    Fix bug in strlen arguments (causing segfault)

--- code/pagers/apager.c
+++ code/pagers/apager.c
@@ -1,18 +1,32 @@
 #include <inttypes.h>
 #include <stdio.h>
 #include <string.h>
+#include <linux/auxvec.h>
 
+#include "loader.h"
 #include "loader_utils.h"
 #include "loader_elf.h"
 
 int main( int argc, char** argv, char** envp )
 {
-  Elf64_auxv_t *auxv;
+  Elf64_auxv_t *auxv = NULL;
   printf( "argv: %#" PRIx64
           "\t contents: %s\n", (uint64_t)argv, *argv );
   ++argv;
   printf( "++argv: %#" PRIx64
           "\t contents: %s\n", (uint64_t)argv, *argv );
+
+  FILE * fp = NULL;
+  char buf[HDR_BUF_SIZE];
+  if (argc > 1) {
+    fp = fopen ((const char*)(*argv), "r");
+    fread( buf, 1, HDR_BUF_SIZE, fp );
+    fclose( fp );
+
+    argc++;
+  }
+
+  printf( "contents: %s", buf );
 
   char** orig_envp = envp;
   int num_env_vars = 0;
@@ -23,9 +37,9 @@ int main( int argc, char** argv, char** envp )
 
   num_env_vars -= 1;  // account for the extra null
 
-  char* final_string_addr = envp[num_env_vars - 1];
+  char* final_string_addr = orig_envp[num_env_vars - 1];
   unsigned long chars_in_final_string = strlen(final_string_addr);
-  printf("strlen of last value @ envp: %lu\n", chars_in_final_string );
+  printf("strlen of last value @ envp: %lu\n", chars_in_final_string );  
   printf("address of final_string_addr + strlen = %lx\n",
          (unsigned long)final_string_addr + chars_in_final_string );
 
@@ -45,6 +59,10 @@ int main( int argc, char** argv, char** envp )
   for (auxv = (Elf64_auxv_t*)envp; auxv->a_type != AT_NULL; auxv++) {
     printf("aux type: %" PRIu64
            "\n", auxv->a_type );
+    if (auxv->a_type == AT_EXECFN) {
+      printf("    exec addr: %#" PRIx64 " \n contents: %s\n",
+             (uint64_t)(auxv->a_un.a_val), (char*)(auxv->a_un.a_val));
+    }
   }
 
   char buffer[17];
